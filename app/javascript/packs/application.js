9f321a03e2130b7e89eff1cdeafb6d88ef3e81b7
IgorBanjac91 <igor.banjac91@gmail.com>
2021-08-23 12:09:30 +0200

    add react to rails

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "./hello_react"
 
 Rails.start()
 Turbolinks.start()
