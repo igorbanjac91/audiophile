0242691952554baa3db2180deaea4dabc847756d
IgorBanjac91 <igor.banjac91@gmail.com>
2021-08-26 13:16:20 +0200

    add categories images

--- app/javascript/packs/components/shared/CategoriesList.jsx
+++ app/javascript/packs/components/shared/CategoriesList.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { ArrowRightIcon } from "./Icons";
 import Earphones from "../../../../assets/images/seeds/image-earphones.png"
+import Headphones from "../../../../assets/images/seeds/image-headphones.png"
+import Speaker from "../../../../assets/images/seeds/image-speakers.png"
 
 const CategoriesList = () => {
 
   return (
     <div className="categories-list">
       <ul>
-        <CategoriesListItem /> 
-        <CategoriesListItem /> 
-        <CategoriesListItem /> 
+        <CategoriesListItem image={Headphones} /> 
+        <CategoriesListItem image={Speaker} /> 
+        <CategoriesListItem image={Earphones} /> 
       </ul>
     </div>
   )
 }
 
-const CategoriesListItem = () => {
+const CategoriesListItem = (props) => {
   
   return (
     <li className="categories-list__item">
-      <img src={Earphones} alt="earphones" />
+      <img src={props.image} alt="earphones" />
       <h3>HEADPHONES</h3>
       <a className="btn-shop">
         <span>SHOP</span>
