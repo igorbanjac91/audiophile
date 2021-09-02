c6fb21be40cb45d227bf5a7f67c4aac4f139b589
IgorBanjac91 <igor.banjac91@gmail.com>
2021-09-02 15:29:26 +0200

    add category page feature tests

---
+++ spec/features/category_page_flow_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper" 
+
+RSpec.describe "Category page flow", type: :feature, js: true do 
+
+  let!(:headphones) { create(:category, name: "headphones") }
+  let!(:xx59) { create(:product, name: "XX59 Headphones", 
+                                 slug: "xx59-haedphones",
+                                 description: "xx59 description", 
+                                 category: headphones) }
+  let!(:xx99) { create(:product, name: "XX99 Headphones", 
+                                 slug: "xx99-headphones",
+                                 description: "xx99 description", 
+                                 category: headphones) }
+  
+
+  describe "category page" do 
+
+    it "shows all the category's products name and description" do 
+      visit categories_path("headphones")
+      headphones.products.each do |product| 
+        expect(page).to have_content(product.name)
+        expect(page).to have_content(product.description)
+      end
+    end
+  end
+
+  describe "see product button link" do 
+
+    it "has a see product btn for each product" do 
+      visit categories_path("headphones")
+      expect(page).to have_link("SEE PRODUCT", href: "/products/#{xx59.slug}")
+      expect(page).to have_link("SEE PRODUCT", href: "/products/#{xx99.slug}")
+    end
+  end
+end
