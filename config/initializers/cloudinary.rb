660b3cf46cb5079d3406f5beb3ad91ffe033db64
IgorBanjac91 <igor.banjac91@gmail.com>
2021-09-14 09:23:14 +0200

    solve cloudinary issue

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dsmap0onp'
+  config.api_key = '914713129356435'
+  config.api_secret = 'yrySl8xWH4G3MPhDaQj2W4S_CiI'
+  config.secure = true
+  config.cdn_subdomain = true
+end
