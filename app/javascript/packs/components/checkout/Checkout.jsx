66cd01b3356e6f5230e5472608c12eb81510ee62
IgorBanjac91 <igor.banjac91@gmail.com>
2021-09-21 15:42:04 +0200

    tidy up code

--- app/javascript/packs/components/checkout/Checkout.jsx
+++ app/javascript/packs/components/checkout/Checkout.jsx
@@ -17,7 +17,6 @@ const Checkout = (props) => {
       <a className="go-back-link">Go Back</a>
       { order && <div className="checkout">
         <CheckoutForm lineItems={lineItems}/>
-        {/* <CheckoutSummary lineItems={lineItems} /> */}
         { success && <SuccessWindow order={order}
                                     lineItems={lineItems}/> }
       </div> }
@@ -25,119 +24,5 @@ const Checkout = (props) => {
   )
 }
 
-const CheckoutSummary = (props) => {
-
-  return (
-    <div className="checkout__summary">
-      <h3>SUMMARY</h3>
-      <SummaryListItems lineItems={props.lineItems} />
-      <SummaryAmount lineItems={props.lineItems} />
-      <PayButton validForm={props.validForm} />
-    </div>
-  )
-}
-
-const SummaryListItems = (props) => {
-
-  let items = props.lineItems.map((item) => {
-    return <SummaryItem key={item.product.id} 
-                        product={item.product}
-                        quantity={item.quantity} />
-  })
-
-  return (
-    <div className="summary-list-items">
-      <ul>
-        {items}
-      </ul>
-    </div>
-  )
-}
-
-const SummaryItem = (props) => {
-
-  return (
-    <li className="summary-item">
-      <div className="summary-item__image" style={{ backgroundImage: `url(${props.product.cart_image})`}}></div>
-      <div className="summary-item__info">
-        <div>
-          <h4>{props.product.cart_name}</h4>
-          <span>$ {props.product.price}</span>
-        </div>
-        <span>x{props.quantity}</span>
-      </div>
-    </li>
-  )
-}
-
-const SummaryAmount = (props) => {
-
-  let total = 0;
-
-  if (props.lineItems) {
-    total = props.lineItems.reduce((prev, lineItem) => {
-      return prev + (lineItem.quantity * lineItem.product.price)
-    }, 0)
-  }
-  let shipping = 50;
-  let vat = total / 100 * 20;
-  let grandTotal = vat + total + shipping;
-  
-  return (
-    <div className="amount">
-      <div className="amount__total">
-        <span>TOTAL</span>
-        <span>$ {total}</span>
-      </div>
-      <div className="amount__shipping">
-        <span>SHIPPING</span>
-        <span>$ 50</span>
-      </div>
-      <div className="amount__vat">
-        <span>VAT (INCLUDED)</span>
-        <span>$ {vat}</span>
-      </div>
-      <div className="amount__grand-total">
-        <span>GRAND TOTAL</span>
-        <span>$ {grandTotal}</span>
-      </div>
-    </div>
-  )
-}
-
-
-const PayButton = (props) => {
-
-  
-  
-  function handleClick(e) {
-    let validForm = true;
-  
-    function validate() {
-      let fields = document.querySelectorAll("input")
-      fields.forEach((field) => {
-        if (field.value == "") {
-          validForm = false;
-        }
-      })
-    }
-
-    validate();
-
-    if (validForm) {
-      let successWindow = document.querySelector(".success-window-container");
-      let body = document.querySelector("body");
-  
-      successWindow.style.display = "block";
-      body.classList.add("drop-shadow");
-    } else {
-
-    }
-  }
-
-  return (
-    <button className="btn-pay" onClick={handleClick}>CONTINUE & PAY</button>
-  )
-}
 
 export default Checkout;
